cc54d07d3750b82fac488ae027b8a8720f78a3ef
Facundo Migliorini <facundo.migliorini@opskins.com>
2020-02-11 13:19:25 -0300

    Adding reactstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
 import Title from './Shared/Title';
+import Board from './Components/Board';
+
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <Title />
-      <header className="App-header">
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Title />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Board />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/Shared/Title/Title.js
+++ src/Shared/Title/Title.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TITLE_APP } from '../../Constants/General';
 
 const Title = () => (
-    <div>
+    <div className="app-title">
         <h1>{TITLE_APP}</h1>
     </div>
 );
