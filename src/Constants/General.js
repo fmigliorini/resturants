36a37092dd4473a9e041b137d5751d25c454635a
Facundo Migliorini <facundo.migliorini@opskins.com>
2020-02-11 13:05:49 -0300

    Initial Structure

---
+++ src/Constants/General.js
@@ -0,0 +1,1 @@
+export const TITLE_APP = "Restaurants Search";
---
+++ src/Shared/Title/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TITLE_APP } from '../../Constants/General';
+
+const Title = () => (
+    <div>
+        <h1>{TITLE_APP}</h1>
+    </div>
+);
+
+export default Title;
